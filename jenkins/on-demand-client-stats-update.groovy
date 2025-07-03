4d5ec49160b7075048acdb89468466b95d7fab11
jholt-bluesageusa <74373868+jholt-bluesageusa@users.noreply.github.com>
2025-07-03 14:12:29 -0400

    Update on-demand-client-stats-update.groovy

--- jenkins/on-demand-client-stats-update.groovy
+++ jenkins/on-demand-client-stats-update.groovy
@@ -81,11 +81,14 @@ pipeline {
                     ]
 
                     def clientConfig = config[params.CLIENT]
-                    
                     docker.image('python:3.11-slim').inside('-u 0:0') {
                         sh """
                             pip install --no-cache-dir --progress-bar off pymysql requests
-                            python3 update_table_stats.py "${clientConfig.host}" "${clientConfig.user}" "${clientConfig.pass}"
+                            python3 update_table_stats.py \
+                                --client ${params.CLIENT} \
+                                --host ${clientConfig.host} \
+                                --user ${clientConfig.user} \
+                                --password ${clientConfig.pass}
                         """
                     }
                 }
